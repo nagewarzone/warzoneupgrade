36ce05fdc24a0cbcc5a0d5861d245fb4fed33356
nagewarzone <nagewarzone@gmail.com>
2025-05-22 02:58:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fetch = require('node-fetch'); // ใช้ส่งข้อความ 
 const admin = require('firebase-admin');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 const serviceAccount = require('./serviceAccountKey.json');
 
